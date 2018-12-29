4733a12f0f564481dd3b94a56647921cfa0ec80d
ThisIsNsh <hadanis.singh@gmail.com>
2018-12-29 23:09:04 +0530

    APIs done

--- assets/food-list.js
+++ assets/food-list.js
@@ -8,79 +8,49 @@ $(document).ready(function(){
   var image = $('#image');
   var locatio = $('#location');
   var mobile = $('#mobile');
-    
-  // var food_name = $('#food_name');
-  // var food_price = $('#food_price');
-  // var food_image = $('#food_image');
 
+
+    
 $("#hotel button").click(function (ev) {
       ev.preventDefault()
+
       if ($(this).attr("value") == "button1") {   
 
-        var menuVal = []
-
-    for(var i=0;i<food_name_list.length();i++){
-      menuVal.push({ food_name:food_name_list[i],food_price:food_price_list[i],food_image:food_image_list[i]});
-    }
+    var hotel =  {name: name.val(), image: image.val(), location: locatio.val(), mobile: mobile.val(), menu: []};
+    var item = []
     
-    var hotel =  {name: name.val(), image: image.val(), location: locatio.val(), mobile: mobile.val(), menu: menuVal };
 
-      $.ajax({
-        type: 'POST',
-        url: '/hotel',
-        data: hotel,
-        success: function(data){
-          location.reload();
+      for(var i=0;i<food_name_list.length;i++){
+        hotel.menu.push({ food_name: food_name_list[i],food_price: food_price_list[i],food_image: food_image_list[i]});
+
+          if (i==food_name_list.length-1){
+            // alert(hotel.menu.length) ;
+            // hotel.menu = item;
+
+            $.ajax({
+              type: 'POST',
+              url: '/hotel',
+              contentType: 'application/json',
+              data: JSON.stringify(hotel),
+              success: function(data){
+                location.reload();
+              }
+            });
+          }
         }
-      });
-
-        alert("First Button is pressed - Form will submit") 
-        $("#hotel").submit();
       }
+
+
       if ($(this).attr("value") == "button2") {
-        food_name_list.push($('#food_name'));
-    food_price_list.push($('#food_price'));
-    food_image_list.push($('#food_image'));
+
+        food_name_list.push($('#food_name').val());
+        food_price_list.push($('#food_price').val());
+        food_image_list.push($('#food_image').val());
     
-        alert("Second button is pressed - Form did not submit")
+        alert(food_name_list.length);
       }
     });
 
-
-
-// $("#hotel").on('click', '#add-hotel', function () {
-//   // add hotel
-
-    
-
-//       return true;
-
-//   }); 
-
-// $("#hotel").on('click', '#add-item', function () {
-//   // add item
-    
-    
-
-//   }); 
-
-
-  // $('#hotel').on('submit', function(){
-
-  //     var hotel =  {name: name.val(), image: image.val(), location: locatio.val(), mobile: mobile.val(), menu: [{food_name: food_name.val(), food_price: food_price.val(),food_image: food_image.val()}] };
-
-  //     $.ajax({
-  //       type: 'POST',
-  //       url: '/hotel',
-  //       data: hotel,
-  //       success: function(data){
-  //         location.reload();
-  //       }
-  //     });
-
-  //     return false;
-
-  // });
 
 
 });
